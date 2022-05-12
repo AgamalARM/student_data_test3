a175220930d9b9acf6c809005be97e82ffc5265b
AgamalARM <75130776+AgamalARM@users.noreply.github.com>
2022-05-12 13:48:24 +0200

    Create test7.py

---
+++ test7.py
@@ -0,0 +1,112 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import pandas as pd
+import numpy as np
+import csv
+
+st.title("Dexcom Students System")
+######### Global variables  ##########
+names = ['Admin','Teacher']
+usernames = ['admin','teacher']
+passwords = ['123','456']
+##################################
+hashed_passwords = stauth.Hasher(passwords).generate()
+authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
+    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login('Login','main')
+
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write('## Welcome *%s*' % (name))
+    st.write("### What is Dataset you want?")
+    select_item = st.radio("Please select Dataset",
+                  ("Add Admin", "Add Teacher", "Add Student","Add Subject"))
+    if select_item == "Add Admin":      ### add admin
+        file1 = open("admin_data.csv")
+        df_admins = pd.DataFrame(file1)  
+        file1.close()
+        
+        admin_id = st.sidebar.text_input("Admin ID")
+        admin_name = st.sidebar.text_input("Admin Name")
+        admin_phone = st.sidebar.text_input("Admin Phone Number")
+        
+        
+        @st.cache(allow_output_mutation=True)
+        def get_data():
+            return []
+            
+        
+        df_admins = pd.DataFrame(get_data())
+        st.write("## Show Admin Dataset")
+        st.write(df_admins)
+        st.write(df_admins.shape)
+        if st.button("Add Admin"):
+            get_data().append({"Admin_ID": admin_id, 
+                           "admin_Name": admin_name, 
+                           "admin_Phone": admin_phone})
+        
+       #####convert df to csv and save it    ###
+        def convert_df(df_admins):
+            return df_admins.to_csv().encode('utf-8')
+        
+        
+        csv1 = convert_df(df_admins)
+        #st.write(csv1)
+        
+        file2 = open('admin_data.csv')
+        df_students.to_csv (r'admin_data.csv', index = False, header=True)
+        file2.close()
+  #####################################################################################
+    elif select_item == "Add Teacher":  ### add teacher
+        pass
+  #######################################################################################
+    elif select_item == "Add Student":    ### add student
+        file1 = open("student_data.csv")
+        df_students = pd.DataFrame(file1)  
+        file1.close()
+        
+        student_id = st.sidebar.text_input("Student ID")
+        student_name = st.sidebar.text_input("Student Name")
+        student_phone = st.sidebar.text_input("Student Phone Number")
+        student_email = st.sidebar.text_input("Student Email")
+        student_class_name = st.sidebar.text_input("Student Class Name")
+        student_subject = st.sidebar.text_input("Student Subject")
+        
+        @st.cache(allow_output_mutation=True)
+        def get_data():
+            return []
+            
+        
+        df_students = pd.DataFrame(get_data())
+        st.write("## Show Student Dataset")
+        st.write(df_students)
+        st.write(df_students.shape)
+        if st.button("Add Student"):
+            get_data().append({"Student_ID": student_id, 
+                           "Student_Name": student_name, 
+                           "Student_Phone": student_phone,
+                           "Student_Email": student_email,
+                           "Student_Class_Name": student_class_name,
+                           "Student_Subject": student_subject})
+        
+       #####convert df to csv and save it    ###
+        def convert_df(df_students):
+            return df_students.to_csv().encode('utf-8')
+        
+        
+        csv1 = convert_df(df_students)
+        #st.write(csv1)
+        
+        file2 = open('student_data.csv')
+        df_students.to_csv (r'student_data.csv', index = False, header=True)
+        file2.close()
+   ####################################################################################
+    elif select_item == "Add Admin":      ### add admin
+        pass
+
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
